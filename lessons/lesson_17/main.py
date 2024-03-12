a62d6187bd3de1890fb9a269b4396594bb0ae400
mr_crow <merdenis91@gmail.com>
2024-03-12 20:17:38 +0100

    lesson_17

---
+++ lessons/lesson_17/main.py
@@ -0,0 +1,15 @@
+import os
+import configparser
+
+# os.path.dirname(os.path.abspath(__file__))
+
+# config = configparser.RawConfigParser().read(path)
+# config.get(section, var_name)
+
+
+
+#
+# def test_eq():
+#
+#     assert 1 == 2, 'My custom assert'
+# driver.implicitly_wait(5)  # буде шукати елемент напротязі 5 секунд. Не бажано для застусування
---
+++ page_objects/locators/login_page.py
@@ -0,0 +1,5 @@
+class Locators:
+
+    username_input_id_locator = 'user-name'  # id of element
+    password_input_xpath_locator = '//input[@data-test="password"]'  # x-path locator
+    login_btn_xpath_locator = '//*[@type="submit"]'  # x-path locator
